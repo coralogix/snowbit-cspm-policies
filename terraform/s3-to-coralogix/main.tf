3a19b07bbc6ce8f125fc5c7104f90f2eb5c033c0
Nir Limor <nir.limor@coralogix.com>
2022-11-29 15:32:29 +0200

    replacing s3-to-coralogix

--- terraform/s3-to-coralogix/main.tf
+++ terraform/s3-to-coralogix/main.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.17.1"
+    }
+  }
+}
 locals {
-  function_name = "${var.functionName}-${random_string.this.result}"
   coralogix_regions = {
     Europe    = "api.coralogix.com"
     Europe2   = "api.eu2.coralogix.com"
@@ -7,95 +14,127 @@ locals {
     Singapore = "api.coralogixsg.com"
     US        = "api.coralogix.us"
   }
-  tags = {
-    Provider = "Coralogix"
-    License  = "Apache-2.0"
+}
+data "aws_region" "this" {}
+data "aws_caller_identity" "current" {}
+data "aws_iam_policy_document" "kms-decrypt" {
+  count = 1
+  statement {
+    sid       = "kmsDecrypt"
+    effect    = "Allow"
+    actions   = ["kms:Decrypt"]
+    resources = [var.kms_arn]
   }
 }
-
-data "aws_region" "this" {}
-
-data "aws_s3_bucket" "this" {
-  bucket = var.s3_bucket_name
+data "aws_iam_policy_document" "s3-bucket-access" {
+  count = 1
+  statement {
+    sid     = "s3BucketAccess"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:GetObjectVersion",
+      "s3:GetLifecycleConfiguration"
+    ]
+    resources = ["arn:aws:s3:::${var.guardduty-s3-bucket}","arn:aws:s3:::${var.guardduty-s3-bucket}/*"]
+  }
 }
-
+resource "aws_iam_policy" "kms-policy" {
+  name   = "kms-policy-${random_string.this.result}"
+  policy = data.aws_iam_policy_document.kms-decrypt[0].json
+}
+resource "aws_iam_policy" "s3-bucket-access" {
+  name = "s3-bucket-access-${random_string.this.result}"
+  policy = data.aws_iam_policy_document.s3-bucket-access[0].json
+}
+resource "aws_iam_policy_attachment" "kms-policy-attachment" {
+  name       = "kms-policy"
+  roles      = [aws_iam_role.lambda-role.name]
+  policy_arn = aws_iam_policy.kms-policy.arn
+}
+resource "aws_iam_policy_attachment" "s3-policy-attachment" {
+  name       = "s3-policy"
+  roles      = [aws_iam_role.lambda-role.name]
+  policy_arn = aws_iam_policy.s3-bucket-access.arn
+}
+resource "aws_iam_role" "lambda-role" {
+  name               = "Lambda-Role-${random_string.this.result}"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+  inline_policy {
+    name   = "basicExecution"
+    policy = jsonencode({
+      Version   = "2012-10-17"
+      Statement = [
+        {
+          "Sid" : "basicExecution",
+          "Effect" : "Allow",
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+}
+resource "aws_lambda_function" "this" {
+  function_name = "s3-to-coralogix"
+  role          = aws_iam_role.lambda-role.arn
+  s3_bucket     = "coralogix-serverless-repo-${data.aws_region.this.name}"
+  s3_key        = "${var.package_name}.zip"
+  runtime       = "nodejs16.x"
+  handler       = "index.handler"
+  architectures = [var.architecture]
+  memory_size   = var.memory_size
+  timeout       = var.timeout
+  environment {
+    variables = {
+      CORALOGIX_URL         = "https://${lookup(local.coralogix_regions, var.coralogix_region, "Europe")}/api/v1/logs"
+      CORALOGIX_BUFFER_SIZE = tostring(var.buffer_size)
+      private_key           = var.private_key
+      app_name              = var.application_name
+      sub_name              = var.subsystem_name
+      blocking_pattern      = var.blocking_pattern
+      debug                 = var.debug
+      newline_pattern       = var.newline_pattern
+      sampling              = var.sampling_rate
+    }
+  }
+}
 resource "random_string" "this" {
-  length  = 12
+  length  = 6
   special = false
 }
-
-module "lambda" {
-  source                 = "terraform-aws-modules/lambda/aws"
-  version                = "3.2.1"
-
-  function_name          = local.function_name
-  description            = "Send logs from S3 bucket to Coralogix."
-  handler                = "index.handler"
-  runtime                = "nodejs16.x"
-  architectures          = [var.architecture]
-  memory_size            = var.memory_size
-  timeout                = var.timeout
-  create_package         = false
-  destination_on_failure = aws_sns_topic.this.arn
-  environment_variables = {
-    CORALOGIX_URL         = "https://${lookup(local.coralogix_regions, var.coralogix_region, "Europe")}/api/v1/logs"
-    CORALOGIX_BUFFER_SIZE = tostring(var.buffer_size)
-    private_key           = var.private_key
-    app_name              = var.application_name
-    sub_name              = var.subsystem_name
-    newline_pattern       = var.newline_pattern
-    blocking_pattern      = var.blocking_pattern
-    sampling              = tostring(var.sampling_rate)
-    debug                 = tostring(var.debug)
-  }
-  s3_existing_package = {
-    bucket = "coralogix-serverless-repo-${data.aws_region.this.name}"
-    key    = "${var.package_name}.zip"
-  }
-  policy_path            = "/coralogix/"
-  role_path              = "/coralogix/"
-  role_name              = "${local.function_name}-Role"
-  role_description       = "Role for ${local.function_name} Lambda Function."
-  create_current_version_allowed_triggers = false
-  create_async_event_config               = true
-  attach_async_event_policy               = true
-  attach_policy_statements                = true
-  policy_statements = {
-    S3 = {
-      effect    = "Allow"
-      actions   = ["s3:GetObject"]
-      resources = ["${data.aws_s3_bucket.this.arn}/*"]
-    }
-  }
-  allowed_triggers = {
-    AllowExecutionFromS3 = {
-      principal  = "s3.amazonaws.com"
-      source_arn = data.aws_s3_bucket.this.arn
-    }
-  }
-  tags = merge(var.tags, local.tags)
+resource "aws_lambda_permission" "invoke" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "s3.amazonaws.com"
+  statement_id = "invoke-${random_string.this.result}"
+  source_arn = "arn:aws:s3:::${var.guardduty-s3-bucket}"
+  source_account = data.aws_caller_identity.current.account_id
 }
-
 resource "aws_s3_bucket_notification" "this" {
-  depends_on      = [ module.lambda ]
-  bucket = data.aws_s3_bucket.this.bucket
+  bucket = var.guardduty-s3-bucket
   lambda_function {
-    lambda_function_arn = module.lambda.lambda_function_arn
+    lambda_function_arn = aws_lambda_function.this.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = var.s3_key_prefix
     filter_suffix       = var.s3_key_suffix
   }
-}
-
-resource "aws_sns_topic" "this" {
-  name_prefix  = "${module.lambda.lambda_function_name}-Failure"
-  display_name = "${module.lambda.lambda_function_name}-Failure"
-  tags         = merge(var.tags, local.tags)
-}
-
-resource "aws_sns_topic_subscription" "this" {
-  count     = var.notification_email != null ? 1 : 0
-  topic_arn = aws_sns_topic.this.arn
-  protocol  = "email"
-  endpoint  = var.notification_email
 }
--- terraform/s3-to-coralogix/variables.tf
+++ terraform/s3-to-coralogix/variables.tf
@@ -1,9 +1,31 @@
+variable "package_name" {
+  description = "The name of the package to use for the function"
+  type        = string
+  default     = "s3"
+}
+
+variable "architecture" {
+  description = "Lambda function architecture"
+  type        = string
+  default     = "x86_64"
+}
+
+variable "memory_size" {
+  description = "Lambda function memory limit"
+  type        = number
+  default     = 1024
+}
+
+variable "timeout" {
+  description = "Lambda function timeout limit"
+  type        = number
+  default     = 300
+}
 variable "coralogix_region" {
   description = "The Coralogix location region, possible options are [Europe, Europe2, India, Singapore, US]"
   type        = string
   default     = "Europe"
 }
-
 variable "private_key" {
   description = "The Coralogix private key which is used to validate your authenticity"
   type        = string
@@ -19,13 +41,6 @@ variable "subsystem_name" {
   description = "The subsystem name of your application"
   type        = string
 }
-
-variable "package_name" {
-  description = "The name of the package to use for the function"
-  type        = string
-  default     = "s3"
-}
-
 variable "newline_pattern" {
   description = "The pattern for lines splitting"
   type        = string
@@ -37,7 +52,6 @@ variable "blocking_pattern" {
   type        = string
   default     = ""
 }
-
 variable "buffer_size" {
   description = "Coralogix logger buffer size"
   type        = number
@@ -56,9 +70,14 @@ variable "debug" {
   default     = false
 }
 
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket to watch"
-  type        = string
+variable "kms_arn" {
+  type = string
+  description = "The ARN for the KMS used for the encryption"
+}
+
+variable "guardduty-s3-bucket" {
+  type = string
+  description = "The s3 that saves the data for GuardDuty"
 }
 
 variable "s3_key_prefix" {
@@ -71,38 +90,4 @@ variable "s3_key_suffix" {
   description = "The S3 path suffix to watch"
   type        = string
   default     = null
-}
-
-variable "memory_size" {
-  description = "Lambda function memory limit"
-  type        = number
-  default     = 1024
-}
-
-variable "timeout" {
-  description = "Lambda function timeout limit"
-  type        = number
-  default     = 300
-}
-
-variable "architecture" {
-  description = "Lambda function architecture"
-  type        = string
-  default     = "x86_64"
-}
-
-variable "notification_email" {
-  description = "Failure notification email address"
-  type        = string
-  default     = null
-}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "functionName" {
-  type = string
 }
